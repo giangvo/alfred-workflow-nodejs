d2e0ec39d6e45458899a2119455ced579610c311
Giang Vo <gvotruong@atlassian.com>
2016-07-10 12:55:42 +1000

    Add travis build

--- test/alfredNodeTest.js
+++ test/alfredNodeTest.js
@@ -3,8 +3,6 @@ var should = Chai.should();
 var assert = Chai.assert;
 var sinon = require("sinon");
 
-var sleep = require("sleep");
-
 var AlfredNode = require("../alfredNode.js");
 var workflow = AlfredNode.workflow;
 var storage = AlfredNode.storage;
@@ -354,29 +352,30 @@ suite("#StorageTest", function() {
         obj.should.equal(obj2);
     });
 
-    test("test set and get item with ttl is not expired", function() {
+    test("test set and get item with ttl is not expired", function(done) {
         var obj = {
             text: "abc"
         };
 
         storage.set("key", obj, 1000); // ttl is 1s
-        sleep.usleep(500000); // 0.5 s
-
-        var obj2 = storage.get("key");
-        obj.should.equal(obj2);
-
+        setTimeout(function() {
+            var obj2 = storage.get("key");
+            obj.should.equal(obj2);
+            done();
+        }, 500);
     });
 
-    test("test set and get item with ttl is expired", function() {
+    test("test set and get item with ttl is expired", function(done) {
         var obj = {
             text: "abc"
         };
 
         storage.set("key", obj, 1000); // ttl is 1s
-        sleep.usleep(1100000); // 1.1 second
-
-        var obj2 = storage.get("key");
-        should.not.exist(obj2);
+        setTimeout(function() {
+            var obj2 = storage.get("key");
+            should.not.exist(obj2);
+            done();
+        }, 1100);
     });
 
     test("test set and get multiple items", function() {
